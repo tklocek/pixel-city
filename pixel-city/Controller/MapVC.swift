57a5e17ecf35adba2e382a033065e796876e4174
Tomasz Klocek <tomek@klocek.info>
2019-11-12 18:20:50 +0100

    Clean project

---
+++ pixel-city/Controller/MapVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  pixel-city
+//
+//  Created by Tomek Klocek on 2019-11-12.
+//  Copyright © 2019 Tomek Klocek. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import AlamofireImage
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
